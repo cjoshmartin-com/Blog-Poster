cbe13d869218f0bd628fa21fb478c4b05629f293
Josh Martin <contact@cjoshmartin.com>
2018-01-07 21:01:08 -0500

    markdown editor added style

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,26 +1,28 @@
 
 import React from 'react';
-import { Text, View, TouchableHighlight, Platform, Button } from 'react-native';
+import { Text, View, TouchableHighlight, Button, TextInput, StyleSheet} from 'react-native';
 
 export default class NewPost extends React.Component {
 
-    state = {isPreview: false };
+    state = {isPreview: false, text: "The Markdown Editor is coming!" };
 
     componentDidMount() {
         this.props.navigation.setParams({ togglePreview: this._togglePreview.bind(this)});
         this.props.navigation.setParams({isEditing: true })
-        //this.setState({isPreview: true})  // quck fix have the same state as isEditing
     }
 
     static navigationOptions = ({ navigation }) => {
         const { params = {} } = navigation.state; 
 
-       let editButton =(<Button title={(params.isEditing)?"Edit" : "Done"} onPress={params.togglePreview ? params.togglePreview : () => null} />) 
-        return { title: 'Compose Your Post',
+        let editButton =(<Button title={(params.isEditing)?"Edit" : "Done"} onPress={params.togglePreview ? params.togglePreview : () => null} />) 
+
+        return { 
+            
+            title: 'Compose Your Post',
             headerRight: editButton,
-       }
+        }
     };
-    
+
     _togglePreview(){
         console.log(this.props.navigation.state.params)
         this.setState({isPreview: !this.state.isPreview })
@@ -29,13 +31,25 @@ export default class NewPost extends React.Component {
     onTextChange(text){
     }
     render(){
-        console.log(this.state.isPreview)
+        console.log(this.state)
         return(
-            <View>
-                <Text> In House Markdown Editor coming soon to a phone near you! </Text>
+            <View style={{flex: 1}}>
+                <TextInput editable = {this.state.isPreview}  multiline = {true} onChangeText={(text) => this.setState({text})} value={this.state.text} style={Styles.BodyField} />
             </View>
         )
-    
+
     }
 
-}
+} // end of class
+
+var Styles = StyleSheet.create({
+    BodyField: {
+        borderColor: 'rgba(82, 194, 175, 1)',
+        borderWidth: 1,
+        flex: 1,
+        padding: 4,
+        paddingLeft: 8,
+        fontSize: 16,
+    }
+
+})
