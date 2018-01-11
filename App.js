d5678da2afa95f503b943b8eea397ec6070a28d0
Josh Martin <contact@cjoshmartin.com>
2018-01-10 22:42:42 -0500

    added auth

--- App.js
+++ App.js
@@ -6,22 +6,36 @@ import Main from './src/Main'
 import NewPost from './src/NewPost.js';
 
 const Runner = () => { // TODO: Change name of this later
+    
+    let blogPosts;
+    auth.onAuthStateChanged((user) => {
+        (user) ? console.log("USER EXIST") : console.log(`USER DOESN'T EXIST`)
+    })
 
+    db.child('blog').on('value', (snapshot)=>{
+        
+        //console.log(snapshot.val());
+    
+    })
+
+const blog_db = db.child('blog');
 
     return {
 
-        Home: { screen: Main },
+        Home: { 
+            screen: Main,
+            params: { blog_db },
+        },
         NewPost: {
             screen: NewPost,
             key: 'main0',
+            params: { blog_db },
         },
     }
 
 }
 
-
-
-const App = StackNavigator(Runner)
+const App = StackNavigator(Runner())
 
 export default App;
 
